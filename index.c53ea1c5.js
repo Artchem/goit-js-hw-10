2a02941991a91edb2eeb88beea5b6b536ad6b0a8
Artchem <127445887+Artchem@users.noreply.github.com>
2023-08-09 20:37:47 +0000

    Deploying to gh-pages from @ Artchem/goit-js-hw-10@1bdddcd676ed63fc223eb988c88fdac4f5d4ec71 🚀

---
+++ index.c53ea1c5.js
@@ -0,0 +1,2 @@
+fetch("https://api.thecatapi.com/v1/breeds?api_key=API_KEY").then((o=>(console.log(o.json()),o))).then((o=>{console.log(o)})).catch((o=>{console.log(o)}));
+//# sourceMappingURL=index.c53ea1c5.js.map
